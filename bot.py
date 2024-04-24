54427b446b0cf2cdee125e042570988aa693942b
samuelmunyoki <samuelmuthembwa@yahoo.com>
2024-04-24 19:48:41 +0300

    mmm

--- bot.py
+++ bot.py
@@ -8,16 +8,13 @@
 #           LICENSE :  MIT                 #
 ############################################
 
-import asyncio
 from telegram import Update, Bot
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, Updater
 
-# async def LogoutBot():
-#     botlogout = Bot(token="6532185799:AAGGRGm1mtRu8uY3_5o2LucTAj6Xaqkdha8")
-#     await botlogout.log_out()
 
-# asyncio.run(LogoutBot())
+# mybot = Bot(token="6532185799:AAFdV6YC_OOeGECDpZqmAHJy8G0avQq9nQY")
 
+# myupdater = Updater(bot=mybot)
 
 async def downloader(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # .bot.get_file(update.message.document)
@@ -30,11 +27,15 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("6532185799:AAGGRGm1mtRu8uY3_5o2LucTAj6Xaqkdha8").base_url("http://localhost:8081/").local_mode(True).build()
-# app = ApplicationBuilder().token("6532185799:AAGGRGm1mtRu8uY3_5o2LucTAj6Xaqkdha8").build()
+app = ApplicationBuilder()
+
+app.base_url(base_url="http://localhost:8081/")
+app.token(token="6532185799:AAFdV6YC_OOeGECDpZqmAHJy8G0avQq9nQY")
+app.local_mode(local_mode=True)
+app.build()
+
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(MessageHandler(filters.Document.FileExtension(".zip", case_sensitive=False) | filters.Document.FileExtension(".7z", case_sensitive=False) | filters.Document.FileExtension(".rar", case_sensitive=False), downloader))
-
 
 app.run_polling()
